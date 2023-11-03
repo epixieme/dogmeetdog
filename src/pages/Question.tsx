7345e75ab65da0e26239d059ddbda779590038a4
epixieme <kirstie_hayes@hotmail.com>
2023-11-03 15:07:05 +0000

    converting questions to typescript

---
+++ src/pages/Question.tsx
@@ -0,0 +1,28 @@
+import Button from "../components/Button";
+
+interface Props {
+  name: string;
+  age: string;
+  breed: string;
+  sex: string;
+  owner: string;
+}
+
+export default function Question1({ name, age, breed, sex, owner }: Props) {
+  return (
+    <form action="" className="question1-container">
+      <label>{name}:</label>
+      <input type="text"></input>
+      <label>{age}:</label>
+      <input type="text"></input>
+      <label>{breed}:</label>
+      <input type="text"></input>
+      <label>{sex}:</label>
+      <input type="text"></input>
+      <label>{owner}:</label>
+      <input type="text"></input>
+
+      <Button route="/question2" />
+    </form>
+  );
+}
--- src/pages/Questions.tsx
+++
@@ -1,8 +0,0 @@
-import Question1 from "./Question1";
-export default function Questions() {
-  return (
-    <section>
-      <Question1 />
-    </section>
-  );
-}
