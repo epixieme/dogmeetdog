d8c3511bcf3b2bad8da71c68491c2b293c726774
epixieme <kirstie_hayes@hotmail.com>
2023-11-13 13:53:00 +0000

    adding apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import {  ApolloClient, InMemoryCache, gql  } from '@apollo/client'
 
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000',
+//   cache: new InMemoryCache(),
+// })
+
+// const query = gql`
+// query {
+//   allDogs {
+//     description,
+//     id,
+//     likes,
+//     name
+//   }
+// }
+// `
+// client.query({ query })
+//   .then((response) => {
+//     console.log(response.data)
+//   })
 
 
 
 function App() {
+  // const result = useQuery(ALL_DOGS)
   return <RouterProvider router={router} />
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "../server";
+// import "../server.old";
+
+import { ApolloClient, InMemoryCache, gql, ApolloProvider,} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
-  <QueryClientProvider client={queryClient}>
+
+ <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
+</QueryClientProvider>
+
 );
