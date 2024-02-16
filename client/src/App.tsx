29efe8871546fbc5bf06ba0a006aa7aa9dc4d9d7
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-02-16 10:09:59 +0000

    fixed JWT issue by moving code to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,42 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { useCookies } from "react-cookie";
+import { setContext } from "@apollo/client/link/context";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
 function App() {
-  return <RouterProvider router={router} />
+  const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql",
+  });
+
+  const [cookies] = useCookies(["token"]);
+
+  const authLink = setContext((_, { headers }) => {
+    const token = cookies.token || "";
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,40 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setContext } from '@apollo/client/link/context';
-import { useCookies } from 'react-cookie';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client'
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
-});
 
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4000',
-//   cache: new InMemoryCache(),
-// })
-const [cookies] = useCookies(['token']);
-
-  const authLink = setContext((_, { headers }) => {
-    const token = cookies.token || '';
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
-
-<ApolloProvider client={client}>
     <App />
-</ApolloProvider>
-
 );
