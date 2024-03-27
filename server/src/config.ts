186360d5b58c01c035592d181e98c7921575cd05
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-03-27 16:01:33 +0000

    added debugging to mongoose

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,16 +1,17 @@
-
 import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
 
-const MONGODB_URI:any = process.env.MONGODB_URI
+const MONGODB_URI: any = process.env.MONGODB_URI;
 
-console.log('connecting to', MONGODB_URI)
+console.log("connecting to", MONGODB_URI);
 
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
-  .catch((error:any) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+  .catch((error: any) => {
+    console.log("error connection to MongoDB:", error.message);
+  });
+mongoose.set("debug", true);
